609047288d9bb61789c92e662ddcd4170b9966b0
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-16 20:07:03 +0100

    Enabled multiline

--- sendMessage.sh
+++ sendMessage.sh
@@ -5,14 +5,23 @@ if [ "$#" -lt 1 ]; then
 	exit 1
 fi
 
+# Copyied just in case ;)
+urlencode() {
+	echo "$*" | sed 's:%:%25:g;s: :%20:g;s:<:%3C:g;s:>:%3E:g;s:#:%23:g;s:{:%7B:g;s:}:%7D:g;s:|:%7C:g;s:\\:%5C:g;s:\^:%5E:g;s:~:%7E:g;s:\[:%5B:g;s:\]:%5D:g;s:`:%60:g;s:;:%3B:g;s:/:%2F:g;s:?:%3F:g;s^:^%3A^g;s:@:%40:g;s:=:%3D:g;s:&:%26:g;s:\$:%24:g;s:\!:%21:g;s:\*:%2A:g'
+}
 
-text=$1
+text="$(echo "$1" | sed 's/"//g')"
 chatId=${2:-$TELEGRAM_CHAT_ID}
 botName=${3:-$TELEGRAM_BOTNAME}
 
+curl -s --output /dev/null "https://api.telegram.org/bot$botName/sendMessage" -F "chat_id=$chatId" -F "text=${text}"
+# -F "parse_mode=html"
+
+: '
 curl --silent --output /dev/null \
   -X POST \
   https://api.telegram.org/bot$botName/sendMessage \
   -H 'Cache-Control: no-cache' \
   -H 'Content-Type: application/json' \
   -d "{ \"chat_id\": $chatId, \"text\": \"$text\" }"
+'
